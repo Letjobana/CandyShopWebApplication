d959a17dbc29d04bfda7a5fd03cac971a67c1225
Suzan Masite <SuzanM@blts.co.za>
2023-01-23 21:44:23 +0200

    Added a Candy controller and view

---
+++ CandyShop/Controllers/CandyController.cs
@@ -0,0 +1,21 @@
+﻿using CandyShop.Repositories.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyShop.Controllers
+{
+    public class CandyController : Controller
+    {
+        private readonly ICandyRepository candyRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public CandyController(ICandyRepository candyRepository,ICategoryRepository categoryRepository)
+        {
+            this.candyRepository = candyRepository;
+            this.categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
